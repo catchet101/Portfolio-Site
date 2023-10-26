5b4cc6c9a54850411f67506e40394486e490226a
Cameron Lee <cal26@sfu.ca>
2023-10-25 21:09:15 -0700

    added a couple things to navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="dark:bg-slate-800">
+      <body className="bg-slate-800">
         <Navbar/>
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 
+// may need to run 'vercel --prod' to get vercel to work
 export default function Home() {
     return (
         <main className="px-6 mx-auto">
-            <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
-                Hello and Welcome, <span className="whitespace-nowrap">I'm <span className="font-bold">Cameron</span></span>
+            <p className="mt-12 mb-12 text-3xl text-center text-white">
+                Hello and Welcome, <span className="whitespace-nowrap">I&#39;m <span className="font-bold">Cameron</span></span>
             </p>
         </main>
     )
