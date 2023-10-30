b94867427322c987b9bb9cc247abf9467d5c9751
Cameron Lee <cal26@sfu.ca>
2023-10-29 21:06:17 -0700

    updated README and added tools section

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Portfolio Site
 
-## Getting Started
+## Description
+Portfolio website to show off my skills and past projects.
 
-First, run the development server:
+## Technologies
+Built in Typescript utilizing the Next.js React framework
+
+## Running Locally
+1. Clone responsitory
+
+2. Ensure npm or equivalent is installed on your system
+
+3. Open a command line and cd into: `/Portfolio-Site/`
+
+4. Run the development server:
 
 ```bash
 npm run dev
@@ -14,13 +25,9 @@ pnpm dev
 bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+5. Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
+<!-- ## Learn More
 
 To learn more about Next.js, take a look at the following resources:
 
@@ -33,4 +40,4 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details. -->
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@
 import React from 'react';
 import ProficiencyBar from './components/proficiency-bar';
 
+// General section variables
+const sectionTailwind = 'w-full md:w-1/3 mt-20 '
+const sectionHeaderTailwind = 'font-bold text-2xl mb-4 text-center '
+
+// Language section variables
 const allBarsColor:string = '#3535bd'
 const languageProficiencies = [
     {language: 'C', barColor: allBarsColor, percentage: 50},
@@ -20,26 +25,34 @@ languageProficiencies.sort((item1, item2) => {
     return item2.percentage - item1.percentage
 })
 
+// Tools section variables
+const tools = [
+    {tool: 'Git', img: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg', link: 'https://git-scm.com/'},
+    {tool: 'Linux', img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg', link: 'https://www.linux.org/'},
+    {tool: 'Python Pandas', img: 'https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/pandas/pandas-original.svg', link: 'https://pandas.pydata.org/'},
+    {tool: 'Python Scikit Learn', img: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg', link: 'https://scikit-learn.org/'},
+]
+
 export default function Home() {
     return (
         <main className='px-6 mx-auto text-white/90'>
             <h1 className='mt-20 text-4xl text-center'>
                 <span className='whitespace-nowrap'>Hi I&#39;m <span className='font-bold'>Cameron</span></span>, Welcome to My Website
             </h1>
-            <div className='flex sm:flex-row flex-col'>
-
-                <section className='flex flex-col text-center sm:w-1/2 mt-20'>
-                    <h2 className='font-bold text-2xl mb-4'>
+            <div className='flex md:flex-row flex-col'>
+                {/* About Me section */}
+                <section className={sectionTailwind + 'flex flex-col text-center'}>
+                    <h2 className={sectionHeaderTailwind}>
                         About Me
                     </h2>
-                    <p className='sm:mx-12 lg:mx-24'>
+                    <p className='md:mx-6'>
                         I&#39;m a 3rd year Computing Science student at SFU seeking my first co-op position. I&#39;m still figuring out what area of Computer Science I
                         want to specialize in so I am open to any experience that will help me learn and grow.
                     </p>
                 </section>
-
-                <section className='sm:w-1/2 mt-20'>
-                    <h2 className='font-bold text-2xl mb-4 text-center'>
+                {/* Languages section */}
+                <section className={sectionTailwind}>
+                    <h2 className={sectionHeaderTailwind}>
                         Languages
                     </h2>
                     <div className='flex justify-center text-center'>
@@ -58,6 +71,28 @@ export default function Home() {
                                         </td>
                                         <td className='px-2'>
                                             {<ProficiencyBar barColor={item.barColor} percentage={item.percentage}/>}
+                                        </td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+                {/* Tools section */}
+                <section className={sectionTailwind}>
+                    <h2 className={sectionHeaderTailwind}>
+                        Other Tools
+                    </h2>
+                    <div className='flex justify-center text-center'>
+                        <table className='border-spacing-1 border-separate'>
+                            <tbody>
+                                {tools.map(item => 
+                                    <tr key={item.tool}>
+                                        <td>
+                                            {item.tool}
+                                        </td>
+                                        <td className='px-2'>
+                                            <a href={item.link} target='_blank' rel='noreferrer'><img src={item.img} alt={item.tool} width={30} height={30}></img></a>
                                         </td>
                                     </tr>
                                 )}
