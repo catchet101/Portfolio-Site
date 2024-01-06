96fe39af12482b8155b45af32f028b34dbac63a2
Cameron Lee <cal26@sfu.ca>
2024-01-05 20:31:14 -0800

    created mobile friendly version of navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,52 @@
+'use client'
 import Link from 'next/link'
 import { FaGithub } from 'react-icons/fa'
 import styles from './Navbar.module.css'
+import { useEffect, useState } from 'react'
 
 const Navbar = () => {
-    return (
-        <nav className='bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10'>
-            <div className='mx-auto flex justify-between flex-col sm:flex-row max-w-3xl'>
+    const [windowWidth, setWindowWidth] = useState(0)
+
+    // Keeps track of window width, used to conditionally render the navbar
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth)
+      }
+  
+      setWindowWidth(window.innerWidth)
+      window.addEventListener('resize', handleResize)
+
+      return () => {
+        window.removeEventListener('resize', handleResize)
+      }
+    }, [])
+
+    if (windowWidth < 640) {
+        return (
+            <nav className='bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10'>
                 <Link href='/' className={styles.navbarLink}>Cameron Lee</Link>
-                <Link href='/projects' className={styles.navbarLink}>Projects</Link>
-                <Link href='https://github.com/cameronlee101' target='_blank' className={styles.navbarLink + ' flex flex-row items-center'}>GitHub&nbsp;<FaGithub/></Link>
-                <Link href='/contact' className={styles.navbarLink}>Contact Me</Link>
-            </div>
-        </nav>
-    )
+                <input className={`${styles.sideMenu} hidden`} type='checkbox' id='sideMenu'/>
+                <label className={styles.hamb} htmlFor='sideMenu'><span className={styles.hambLine}></span></label>
+                <ul className={styles.menu}>
+                    <li><Link href='/projects' className={styles.navbarLink}>Projects</Link></li>
+                    <li><Link href='https://github.com/cameronlee101' target='_blank' className={styles.navbarLink}>GitHub&nbsp;<FaGithub/></Link></li>
+                    <li><Link href='/contact' target='' className={styles.navbarLink}>Contact Me</Link></li>
+                </ul>
+            </nav>
+        )
+    }
+    else {
+        return (
+            <nav className='bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10'>
+                <div className='mx-auto flex justify-between flex-col sm:flex-row max-w-3xl'>              
+                    <Link href='/' className={styles.navbarLink}>Cameron Lee</Link>
+                    <Link href='/projects' className={styles.navbarLink}>Projects</Link>
+                    <Link href='https://github.com/cameronlee101' target='_blank' className={styles.navbarLink}>GitHub&nbsp;<FaGithub/></Link>
+                    <Link href='/contact' target='' className={styles.navbarLink}>Contact Me</Link>                
+                </div>
+            </nav>
+        )
+    }
 }
 
 export default Navbar
