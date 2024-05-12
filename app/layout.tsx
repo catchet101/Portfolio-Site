e305144697fbee39c456872f408e1aab18142c74
Cameron Lee <cal26@sfu.ca>
2024-05-12 14:13:55 -0700

    deleted profile site and installed prettier with husky

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,24 @@
-import Navbar from './components/navbar/Navbar'
-import './globals.css'
-import { Roboto } from 'next/font/google'
+import "./globals.css";
+import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
-  weight: '300',
-  subsets: ['latin'],
-})
+  weight: "300",
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'Portfolio Site',
-  description: 'Cameron\'s Portfolio Site',
-}
+  title: "Portfolio Site",
+  description: "Cameron's Portfolio Site",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={`${roboto.className} bg-slate-700`}>
-        <Navbar/>
-        {children}
-      </body>
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,10 @@
 // note: may need to run 'vercel --prod' after pushing to main to get vercel to work
-import React from 'react';
-import LanguagesTable from './components/main-page/LanguagesTable';
-import ToolsTable from './components/main-page/ToolsTable';
-import styles from './app.module.css'
+import React from "react";
 
 export default function Home() {
-    return (
-        <main className='main'>
-            <h1 className='page-header'>
-                <span className='whitespace-nowrap'>Hi I&#39;m <span className='font-bold'>Cameron</span></span>, Welcome to My Website
-            </h1>
-            <section className='flex flex-col mt-0 items-center'>
-                {/* About Me section */}
-                <section className={styles.sectionHalves + ' flex flex-col text-center'}>
-                    <h2 className='section-header'>
-                        About Me
-                    </h2>
-                    <p className='lg:mx-6 leading-8'>
-                        I&#39;m a 3rd year Computing Science student at SFU seeking my first co-op position. I&#39;m still figuring out what area of Computer Science 
-                        I want to specialize in so I am open to any experience that will help me learn and grow.<br/>
-                    </p>
-                </section>
-                {/* Languages section */}
-                <section className={styles.sectionHalves}>
-                    <LanguagesTable/>
-                </section>
-                {/* Tools section */}
-                <section className={styles.sectionHalves}>
-                    <ToolsTable/>
-                </section>
-            </section>
-        </main>
-    )
+  return (
+    <main>
+      <p>hi there</p>
+    </main>
+  );
 }
